5c89f4b9aeb497d31d3939e76c23ae41ba755a90
sanjaySDET <sanjaybabusdet@gmail.com>
2024-10-22 16:46:06 +0530

    Merge branch 'master' of https://github.com/sanjaySDET/googleDrive.git

--- GoogleDrive/src/test/java/Demo/TestScript2.java
+++ GoogleDrive/src/test/java/Demo/TestScript2.java
@@ -5,10 +5,7 @@ public class TestScript2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//welcome
-		
 		System.out.println("hello");
-		
-		//completed
+	
 	}
 }
